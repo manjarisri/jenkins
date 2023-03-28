33f4b9cb3cdae4371b2930fb61b20f3028a2886b
Manjari Srivastav <126674100+manjarisri@users.noreply.github.com>
2023-03-28 12:07:50 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM mysql:latest
+
+ENV MYSQL_ROOT_PASSWORD=root
+
+COPY ./student.sql /docker-entrypoint-initdb.d/
+
---
+++ student.sql
@@ -0,0 +1,11 @@
+
+create database student;
+
+use student;
+
+
+create table student(rno int(5), name varchar(20));
+
+insert into student values(1,"manjari");
+
+insert into student values(2,"shubham");
